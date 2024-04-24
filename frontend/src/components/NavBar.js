7846fa93b45d652446b13268ecb7e45bc7bc645c
Sulabh Katila <sulabhkatila@gmail.com>
2024-04-24 13:07:53 -0400

    fix link in collapsed navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -50,7 +50,7 @@ export default function NavBar() {
         </li>
         <li>
           <Link to="/challange" className={`${navLinkClasses}`}>
-            Challenge
+            Challange
           </Link>
         </li>
       </ul>
@@ -76,7 +76,7 @@ export default function NavBar() {
           </li>
           <li>
             <Link
-              to="/challenge"
+              to="/challange"
               className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
             >
               Challange
